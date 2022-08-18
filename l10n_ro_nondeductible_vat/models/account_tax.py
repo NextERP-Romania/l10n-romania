037bfc11521575feb5c73ca576e620c829962a3f
Cojocaru Marcel <marcel.cojocaru@gmail.com>
2022-08-18 14:49:02 +0300

    isolate l10n_ro_nondeductible_vat

--- l10n_ro_nondeductible_vat/models/account_tax.py
+++ l10n_ro_nondeductible_vat/models/account_tax.py
@@ -6,28 +6,33 @@ from odoo import api, fields, models
 
 
 class AccountTaxExtend(models.Model):
-    _inherit = "account.tax"
+    _name = "account.tax"
+    _inherit = ["account.tax", "l10n.ro.mixin"]
 
-    nondeductible_tax_id = fields.Many2one("account.tax", copy=False)
-    is_nondeductible = fields.Boolean(
-        compute="_compute_boolean_nondeductible", store=True
+    l10n_ro_nondeductible_tax_id = fields.Many2one("account.tax", copy=False)
+    l10n_ro_is_nondeductible = fields.Boolean(
+        compute="_compute_boolean_l10n_ro_nondeductible", store=True
     )
 
     @api.depends("invoice_repartition_line_ids", "refund_repartition_line_ids")
-    def _compute_boolean_nondeductible(self):
-        for record in self:
-            record.is_nondeductible = any(
-                record.invoice_repartition_line_ids.mapped("nondeductible")
-                + record.refund_repartition_line_ids.mapped("nondeductible")
+    def _compute_boolean_l10n_ro_nondeductible(self):
+        ro_recs = self.filtered("is_l10n_ro_record")
+        for record in ro_recs:
+            record.l10n_ro_is_nondeductible = any(
+                record.invoice_repartition_line_ids.mapped("l10n_ro_nondeductible")
+                + record.refund_repartition_line_ids.mapped("l10n_ro_nondeductible")
             )
+        for record in self - ro_recs:
+            record.l10n_ro_is_nondeductible = False
 
 
 class AccountTaxRepartitionLineExtend(models.Model):
-    _inherit = "account.tax.repartition.line"
+    _name = "account.tax.repartition.line"
+    _inherit = ["account.tax.repartition.line", "l10n.ro.mixin"]
 
-    nondeductible = fields.Boolean()
-    exclude_from_stock = fields.Boolean()
-    skip_cash_basis_account_switch = fields.Boolean(
+    l10n_ro_nondeductible = fields.Boolean()
+    l10n_ro_exclude_from_stock = fields.Boolean()
+    l10n_ro_skip_cash_basis_account_switch = fields.Boolean(
         help="If checked, then it doesn't change expense account"
         " in the tax line for invoices, and it set 44283 instead of expense"
         " account for the journal entry created at payment reconciliation"
